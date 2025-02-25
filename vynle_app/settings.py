b5cbe880f42fc0a1b478cc1b781b501f43d99895
Ty Janik <tyjanik44@gmail.com>
2025-02-25 13:25:14 -0500

    Removing commented out code

--- vynle_app/settings.py
+++ vynle_app/settings.py
@@ -152,16 +152,7 @@ USE_I18N = True
 USE_L10N = True
 USE_TZ = True
 
-# ===== Static files (CSS, JavaScript, Images) ===== #
-
 STATIC_URL = '/static/'
-
-# STATICFILES_DIRS = [
-#     os.path.join(BASE_DIR, "static"),
-# ]
-
-# Define the directory where collected static files will be stored
-# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 APPEND_SLASH = True
 
