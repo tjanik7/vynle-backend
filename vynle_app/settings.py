ae62dc587aa6f216a4125587cf17c439ee48efc1
Ty Janik <tyjanik44@gmail.com>
2025-02-25 13:22:40 -0500

    Add static files back to settings.py since they are necessary for serving Django admin page

--- vynle_app/settings.py
+++ vynle_app/settings.py
@@ -53,6 +53,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'django.contrib.staticfiles',
     'rest_framework',
     'users.apps.UsersConfig',
     'posts.apps.PostsConfig',
@@ -150,6 +151,17 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 USE_L10N = True
 USE_TZ = True
+
+# ===== Static files (CSS, JavaScript, Images) ===== #
+
+STATIC_URL = '/static/'
+
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, "static"),
+# ]
+
+# Define the directory where collected static files will be stored
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 APPEND_SLASH = True
 
